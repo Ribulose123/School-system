c8a2adc3ac36160d70446028da5d063ff6a76973
Ribulose <okposiokparobor@gmail.com>
2025-09-11 17:52:16 +0100

    Teacher Migiration

---
+++ Schoolsystem.Domain/Teacher.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Schoolsystem.Domain
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string TeacherName { get; set; } = default!;
+        public GenderType Gender { get; set; }
+        public string? Qulification { get; set; }
+        public string Email { get; set; } = default!;
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ Schoolsystem.Persistence/Migrations/20250911164416_#TeacherMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schoolsystem.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class TeacherMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TeacherName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false),
+                    Qulification = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teachers");
+        }
+    }
+}
